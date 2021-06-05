340f1381070bcf327366cd4f375718f8ead10715
Cotemustis <jose.i.villablanca@gmail.com>
2021-06-05 14:25:59 -0400

    List fragmet connected to viewModel

--- app/src/main/java/cl/cotemustis/rickandmorty/presentation/list/ListFragment.kt
+++ app/src/main/java/cl/cotemustis/rickandmorty/presentation/list/ListFragment.kt
@@ -7,8 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import cl.cotemustis.rickandmorty.R
+import cl.cotemustis.rickandmorty.data.model.CharactersResponseData
+import cl.cotemustis.rickandmorty.data.utils.Resource
+import cl.cotemustis.rickandmorty.data.utils.Status
+import cl.cotemustis.rickandmorty.databinding.RmListFragmentBinding
+import cl.cotemustis.rickandmorty.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ListFragment : Fragment() {
@@ -17,17 +24,95 @@ class ListFragment : Fragment() {
         fun newInstance() = ListFragment()
     }
 
+    private var _binding: RmListFragmentBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: ListViewModel by viewModels()
+
+    @Inject
+    lateinit var adapter: RmAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.rm_list_fragment, container, false)
+    ): View {
+        _binding = RmListFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.retrieveCharacters()
+        setObservers()
+        setListeners()
+    }
+
+    private fun setListeners() {
+
+
+    }
+
+    private fun setObservers() {
+        viewModel.charactersListStatus.observe(viewLifecycleOwner, { event ->
+            event.getContentIfNotHandled()?.let {
+                when(it.status){
+                    Status.LOADING ->{
+                        // Show Loading
+                        showLoadingState(false)
+                    }
+                    Status.ERROR -> {
+                        //Show error message
+                        showErrorState(it.message)
+                    }
+                    Status.SUCCESS -> {
+                        // Show data and validate empty status
+                        setData(it.data)
+                    }
+
+                }
+            }
+        })
+
+    }
+
+    private fun setData(data: CharactersResponseData?) {
+        if(data == null){
+            showErrorState("Some problem occurred")
+            return
+        }
+
+        if( data.characterList.isNullOrEmpty()){
+            showEmptyStatus()
+            return
+        }
+        binding.listMainGroup.visible = true
+        binding.listErrorGroup.visible = false
+        binding.listEmptyGroup.visible = false
+        binding.listProgressGroup.visible = false
+        adapter.characters = data.characterList
+        binding.charactersRecyclerView.adapter = adapter
+    }
+
+    private fun showErrorState(message: String?) {
+        binding.listEmptyGroup.visible = false
+        binding.listErrorGroup.visible = true
+        binding.listMainGroup.visible = false
+        binding.listProgressGroup.visible = false
+    }
+
+    private fun showLoadingState(isRefreshing: Boolean) {
+        binding.listEmptyGroup.visible = false
+        binding.listErrorGroup.visible = false
+        binding.listMainGroup.visible = false
+        binding.listProgressGroup.visible = true
+    }
+
+    private fun showEmptyStatus() {
+        binding.listEmptyGroup.visible = true
+        binding.listErrorGroup.visible = false
+        binding.listMainGroup.visible = false
+        binding.listProgressGroup.visible = false
     }
 
 }
